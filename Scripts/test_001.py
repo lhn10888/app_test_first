58183eb4eff51a0166278b81aeea7878d6abac3f
lhn10888 <lhn668888@163.com>
2018-07-06 21:39:54 +0800

    txt input

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -1,4 +1,11 @@
-import  allure
+import allure,pytest
 class Test:
+    @allure.step(title="第一个测试")
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test01(self):
+        allure.attach("描述","这是第一个描述")
         assert  1
+    @allure.step(title="第二个测试")
+    def test02(self):
+        allure.attach("描述","这是第二个描述")
+        assert  0
